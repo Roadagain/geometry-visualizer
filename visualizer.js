0b59ff0ddda20b07031c3620cc851eb035325aec
Roadagain <ringoh72@gmail.com>
2016-06-26 22:05:01 +0900

    circleに塗りつぶし機能を追加
    任意に選べる

--- visualizer.js
+++ visualizer.js
@@ -8,11 +8,14 @@ function line(x1, y1, x2, y2){
   c.stroke();
 }
 
-function circle(x, y, r){
+function circle(x, y, r, fill){
   'use strict';
 
   var c = document.getElementById('c').getContext('2d');
   c.beginPath();
   c.arc(x, y, r, 0, 7, false);
+  if (fill){
+    c.fill();
+  }
   c.stroke();
 }
