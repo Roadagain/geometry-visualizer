21fba6a38d48dee0401ae6ee2352a304df999f1f
Roadagain <ringoh72@gmail.com>
2016-06-26 22:12:57 +0900

    長方形の描画を実装
    左上座標と右下座標を渡す

--- visualizer.js
+++ visualizer.js
@@ -34,3 +34,19 @@ function triangle(x1, y1, x2, y2, x3, y3, fill){
   }
   c.stroke();
 }
+
+function rectangle(x1, y1, x2, y2, fill){
+  'use strict';
+
+  var c = document.getElementById('c').getContext('2d');
+  c.beginPath();
+  c.moveTo(x1, y1);
+  c.lineTo(x2, y1);
+  c.lineTo(x2, y2);
+  c.lineTo(x1, y2);
+  c.lineTo(x1, y1);
+  if (fill){
+    c.fill();
+  }
+  c.stroke();
+}
