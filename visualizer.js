34ba896645b6ddebbb35b7c7de47c65b4b5691fc
Roadagain <ringoh72@gmail.com>
2016-07-24 23:06:11 +0900

    線を描画するフォームを追加

--- visualizer.js
+++ visualizer.js
@@ -50,3 +50,14 @@ function rectangle(x1, y1, x2, y2, fill){
   }
   c.stroke();
 }
+(function(){
+  window.addEventListener('load', function(){
+    document.getElementById('lButton').addEventListener('click', function(){
+      var x1 = document.getElementById('lX1').value;
+      var y1 = document.getElementById('lY1').value;
+      var x2 = document.getElementById('lX2').value;
+      var y2 = document.getElementById('lY2').value;
+      line(x1, y1, x2, y2);
+    });
+  });
+})();
